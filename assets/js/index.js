216922dba1f0c0ae91d5d0cdea38d083bb08b595
Dan Gold <dan97gold@hotmail.com>
2019-03-29 02:57:07 +0000

    Modal Info

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,19 @@
-$(document).ready(function(){
-    var controller = new ScrollMagic.Controller({
-        globalSceneOptions: {
-            triggerHook: 'onCenter'
-        }
-    });
+// $(document).ready(function(){
+//     var controller = new ScrollMagic.Controller({
+//         globalSceneOptions: {
+//             triggerHook: 'onCenter'
+//         }
+//     });
 
-    var scene = new ScrollMagic.Scene({triggerElement: '.hero__title', duration: 500, offset: 1000})
-    .setTween('.title--right', 1, {translateX: '-100%'})
-    .addTo(controller);
+//     var scene = new ScrollMagic.Scene({triggerElement: '.hero__title', duration: 500, offset: 1000})
+//     .setTween('.title--right', 1, {translateX: '-100%'})
+//     .addTo(controller);
 
-    window.addEventListener('scroll', function(){
-        document.querySelector(".title--right").style.transform = `translateX(${-(Math.round(scrollY))}px)`;
 
-        document.querySelector(".title--left").style.transform = `translateX(${(Math.round(scrollY))}px)`;
-    });
+// });
+
+window.addEventListener('scroll', function(){
+    document.querySelector(".title--right").style.transform = `translateX(${(Math.round(scrollY))}px)`;
+
+    document.querySelector(".title--left").style.transform = `translateX(-${(Math.round(scrollY))}px)`;
 });
