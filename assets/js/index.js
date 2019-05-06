73e834abc4d60b41472ebe9582456c3c71145c20
Dan Gold <dan97gold@hotmail.com>
2019-05-06 23:55:58 +0100

    Feedback tweaks
    - Change homepage text to scroll instead of fixed, and also add dropcaps
    - Remove outline to homepage nav numbers
    - Change personal link from twitter to portfolio instead
    - Increase base paragraph font size

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,10 @@
 $(document).ready(function(){
-    var controller = new ScrollMagic.Controller(
-        {
-            globalSceneOptions: {
-                triggerHook: 'onCenter'
-            }
-        }
-    );
+    var controller = new ScrollMagic.Controller();
 
     $('.text__block').each(function(){
         var tween = TweenMax.to(this, 1, {className: "+=fade-in"});
-        // var tween = TweenMax.fromTo(this, 1, {opacity:0, scale:0}, {opacity:1, scale:1});
 
-        var scene = new ScrollMagic.Scene({triggerElement: this, duration: $(window).height()/1.5})
+        var scene = new ScrollMagic.Scene({triggerElement: this, duration: $(window).height()/2})
         .setTween(tween)
         .addTo(controller);
         }
